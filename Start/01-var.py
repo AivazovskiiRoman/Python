fe57b2d8bba17571a0f0724712716f68634dfddd
Roman Himadinov <aivazovskiiroman@gmail.com>
2023-01-01 18:25:12 -0800

    variables

---
+++ Start/01-var.py
@@ -0,0 +1,49 @@
+# variables
+
+print("YO!")
+a = "BOO"
+b = "ZOO"
+print(a * 5)
+print(len(b))
+print(a.lower())
+print(a.upper())
+print(bool(1))
+print(bool(0))
+x = True
+print(x)
+print(id(1))
+print(id(2))
+print(id(None))
+print(1 == 1)
+print(1 == 2)
+print(1 > 1)
+print(1 > 2)
+print(1 < 2)
+print(1 != 2)
+print(1 != 1)
+print(id("apple"))
+print(id("Apple"))
+print(id("apple"))
+
+# legal variable names:
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# multiple variables in one line:
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# output multiple variables
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
