829d0db972227d324e604bccc3eecc8f031daab7
Roman Himadinov <aivazovskiiroman@gmail.com>
2023-01-07 18:45:31 -0800

    set

--- Start/07-set.py
+++ Start/07-set.py
@@ -90,3 +90,41 @@ my_set_11 = {"aaa", "bbb", "ccc", True, 1, 2}
 print(my_set_11)  # {True, 2, 'ccc', 'bbb', 'aaa'}
 
 print("--------------------")
+
+# intersection_update(element)
+
+my_set_12 = {"aaa", "bbb", "ccc"}
+my_set_12_2 = {"ggg", "mmm", "aaa"}
+my_set_12.intersection_update(my_set_12_2)
+print(my_set_12)  # {'aaa'}
+
+print("--------------------")
+
+# symmetric_difference_update(element)
+
+my_set_13 = {"aaa", "bbb", "ccc"}
+my_set_13_2 = {"ggg", "mmm", "aaa"}
+my_set_13.symmetric_difference_update(my_set_13_2)
+print(my_set_13)  # {'bbb', 'ccc', 'ggg', 'mmm'}
+
+print("--------------------")
+
+# Set Methods
+
+# add()	Adds an element to the set
+# clear()	Removes all the elements from the set
+# copy()	Returns a copy of the set
+# difference()	Returns a set containing the difference between two or more sets
+# difference_update()	Removes the items in this set that are also included in another, specified set
+# discard()	Remove the specified item
+# intersection()	Returns a set, that is the intersection of two other sets
+# intersection_update()	Removes the items in this set that are not present in other, specified set(s)
+# isdisjoint()	Returns whether two sets have a intersection or not
+# issubset()	Returns whether another set contains this set or not
+# issuperset()	Returns whether this set contains another set or not
+# pop()	Removes an element from the set
+# remove()	Removes the specified element
+# symmetric_difference()	Returns a set with the symmetric differences of two sets
+# symmetric_difference_update()	inserts the symmetric differences from this set and another
+# union()	Return a set containing the union of sets
+# update()	Update the set with the union of this set and others
